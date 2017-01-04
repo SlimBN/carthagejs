07af74e668a445afcc49ec17bc4bb2edc48deaa6
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2017-01-04 23:30:02 +0100

    advancing on the api

---
+++ cli/commands/implant.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Command = require('cmnd').Command;
+
+class ServerCommand extends Command {
+
+  constructor() {
+
+    super('implant');
+
+  }
+
+  help() {
+
+    return {
+      description: 'Implant a continuous instance of your Carthage Server'
+    };
+
+  }
+
+  run(params, callback) {
+
+    let spawn = require('cross-spawn-async');
+    let child = spawn('pm2 start',  ['cluster.js'], {stdio: 'inherit'});
+
+    process.on('exit', function() {
+      child && child.kill();
+    });
+
+  }
+
+}
+
+module.exports = ServerCommand;
---
+++ update_package.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "advancing on the api"
+git push
+npm publish
